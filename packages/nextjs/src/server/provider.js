a1bca9b75733cdee6de2af1468bfa2389f7b2cf6
Johnny Klironomos <ioannis.klironomos@gmail.com>
2025-02-18 12:40:12 +0000

    Pass custom cookie storage.

--- packages/nextjs/src/server/provider.js
+++ packages/nextjs/src/server/provider.js
@@ -1,18 +1,35 @@
+import {cookies} from "next/headers"
 import React from 'react'
 import ClientProvider from '../client/provider'
+import makeSDK from '@xaiku/node'
 
 const getState = React.cache(async () => {
-  const data = getData()
+  const cookieStore = await cookies()
+  const custom = {
+    name: "cookie",
+    supported: true,
+    get: (key) => cookieStore.get(key),
+    set: (key, value) => cookieStore.set(key, value),
+    delete: (key) => cookieStore.delete(key)
+  }
 
-  return data
+  const sdk = makeSDK({
+    pkey: process.env.NEXT_PUBLIC_XAIKU_API_KEY,
+    store: {custom}
+  })
+
+  const projects = await fetchProjects(sdk)
+  return { projects }
 })
 
 const Provider = async ({ children, ...rest }) => {
+  const initialState = rest.initialState || (await getState())
+
   return (
     <ClientProvider
       apiKey={process.env.NEXT_PUBLIC_XAIKU_API_KEY}
       {...rest}
-      initialState={rest.initialState || (await getState())}
+      initialState={initialState}
     >
       {children}
     </ClientProvider>
